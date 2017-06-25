8425d7add3bcb97fce3f09f1a3ef850d95fca6b6
Jacek <jacek.paszek@onet.pl>
2017-06-25 21:33:45 +0200

    js and css modifications - now it's working

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,16 @@
-$(function(){
+$(function() {
 	var carouselList = $("#carousel ul");
 	setInterval(changeSlide, 3000);
-	carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-});
 
-$(moveFirstSlide).function() {
-    var firstItem = carouselList.find("li:first");
-    var lastItem = carouselList.find("li:last");
-    lastItem.after(firstItem);
-    carouselList.css({marginLeft:0});
-}
+	function changeSlide() {
+		$("carouselList").animate({marginLeft:'-400'}, 2000, moveFirstSlide());
+	}
+
+    function moveFirstSlide() {
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");
+        lastItem.after(firstItem);
+        carouselList.css({marginLeft:0});
+    }
+})
+
